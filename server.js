6d3a5aa5764c98c7c1a384b8910ac3e575f779fd
retrydp <thetemon@gmail.com>
2023-05-03 16:48:15 +0300

    terminal colors, logging to logfile

--- server.js
+++ server.js
@@ -1,31 +1,67 @@
 const tmi = require('tmi.js');
 const env = require('dotenv');
+const fs = require('fs');
 env.config();
+
+const TERM_COLORS = {
+  black: 30,
+  red: 31,
+  green: 32,
+  yellow: 33,
+  blue: 34,
+  magenta: 35,
+  cyan: 36,
+  gray: 90,
+  lightRed: 91,
+  lightGray: 37,
+  lightGreen: 92,
+  lightYellow: 93,
+  lightBlue: 94,
+  lightMagenta: 95,
+  lightCyan: 96,
+  white: 97,
+};
+const LOG_FILE = './logs/log.txt';
+
 function colorize(color, output) {
   return ['\033[', color, 'm', output, '\033[0m'].join('');
 }
+
+const currentTime = () =>
+  new Date().toLocaleTimeString('en-US', { hour12: false });
+
 const client = new tmi.Client({
-  options: { debug: true },
+  options: { debug: false },
   identity: {
     username: process.env.TWITCH_USERNAME,
     password: process.env.TWITCH_TOKEN,
   },
-  channels: ['kykla66'],
+  channels: ['cohhcarnage'],
 });
+
 client.connect().catch(console.error);
-const currentTime = () =>
-  new Date().toLocaleTimeString('en-US', { hour12: false });
+
 client.on('message', (channel, tags, message, self) => {
   if (self) return;
-  // if (message.toLowerCase() === '!hello') {
-  //   client.say(channel, `@${tags.username}, heya!`);
-  // }
-  const nameTag = `<${currentTime()}>[channel:${channel}] ${tags.username} ${
-    tags['message-type']
-  }: `;
-  const coloredTag = colorize(36, nameTag);
-  process.stdout.write(coloredTag);
-  console.log(message);
+
+  const formatted = `<${currentTime()}>[channel:${channel}] ${colorize(
+    TERM_COLORS.red,
+    tags.username
+  )} ${tags['message-type']}: ${message}\n`;
+
+  process.stdout.write(formatted);
+  if (tags.username === 'cohhilitionbot') {
+    fs.appendFile(
+      LOG_FILE,
+      `<${currentTime()}>[channel:${channel}] ${tags.username} ${message}\n`,
+      (err) => {
+        if (err) {
+          console.log(err);
+          process.exit(0);
+        }
+      }
+    );
+  }
 });
 client.on('redeem', (channel, username, rewardType, tags, message) => {
   console.log(

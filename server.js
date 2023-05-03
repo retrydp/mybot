9fb585206a6d2bc3a85ba38e28d76f520e848637
retrydp <thetemon@gmail.com>
2023-05-03 18:00:17 +0300

    trying oop

--- server.js
+++ server.js
@@ -3,89 +3,172 @@ const env = require('dotenv');
 const fs = require('fs');
 env.config();
 
-const TERM_COLORS = {
-  black: 30,
-  red: 31,
-  green: 32,
-  yellow: 33,
-  blue: 34,
-  magenta: 35,
-  cyan: 36,
-  gray: 90,
-  lightRed: 91,
-  lightGray: 37,
-  lightGreen: 92,
-  lightYellow: 93,
-  lightBlue: 94,
-  lightMagenta: 95,
-  lightCyan: 96,
-  white: 97,
-};
-const LOG_FILE = './logs/log.txt';
-const LOG_TARGETS = ['cohhilitionbot', 'suzunahara'];
+class MyBot {
+  TERM_COLORS = {
+    white: 97,
+    black: 30,
+    red: 31,
+    green: 32,
+    yellow: 33,
+    blue: 34,
+    magenta: 35,
+    cyan: 36,
+    gray: 90,
+    lightRed: 91,
+    lightGray: 37,
+    lightGreen: 92,
+    lightYellow: 93,
+    lightBlue: 94,
+    lightMagenta: 95,
+    lightCyan: 96,
+  };
+  LOG_TARGETS = ['byme69', 'blackgeneralgenagenerator', 'latatatum'];
+  CHANNELS = ['k0smos95'];
 
-function colorize(color, output) {
-  return ['\033[', color, 'm', output, '\033[0m'].join('');
+  constructor() {
+    const client = new tmi.Client({
+      options: { debug: false },
+      identity: {
+        username: process.env.TWITCH_USERNAME,
+        password: process.env.TWITCH_TOKEN,
+      },
+      channels: this.CHANNELS,
+    });
+
+    client.connect().catch(console.error);
+
+    client.on('message', (...args) => {
+      if (args.at(-1)) return;
+      this.terminalLog(...args);
+      this.fileLog(...args);
+    });
+  }
+
+  logFilePathHandler(channelName) {
+    return `./logs/${channelName}.txt`;
+  }
+
+  colorize(color, output) {
+    return ['\x1b[', color, 'm', output, '\x1b[0m'].join('');
+  }
+
+  currentTime() {
+    return new Date().toLocaleTimeString('en-US', { hour12: false });
+  }
+
+  fileLog(channel, tags, message) {
+    if (this.LOG_TARGETS.includes(tags.username)) {
+      fs.appendFile(
+        this.logFilePathHandler(channel),
+        `<${this.currentTime()}>[channel:${channel}] ${
+          tags.username
+        } ${message}\n`,
+        (err) => {
+          if (err) {
+            console.log(err);
+            process.exit(0);
+          }
+        }
+      );
+    }
+  }
+
+  terminalLog(channel, tags, message) {
+    const formatted = `<${this.currentTime()}>[channel:${channel}] ${this.colorize(
+      this.TERM_COLORS.red,
+      tags.username
+    )} ${tags['message-type']}: ${message}\n`;
+
+    process.stdout.write(formatted);
+  }
 }
 
-const currentTime = () =>
-  new Date().toLocaleTimeString('en-US', { hour12: false });
+const bot = new MyBot();
 
-const client = new tmi.Client({
-  options: { debug: false },
-  identity: {
-    username: process.env.TWITCH_USERNAME,
-    password: process.env.TWITCH_TOKEN,
-  },
-  channels: ['cohhcarnage'],
-});
+// const TERM_COLORS = {
+//   white: 97,
+//   black: 30,
+//   red: 31,
+//   green: 32,
+//   yellow: 33,
+//   blue: 34,
+//   magenta: 35,
+//   cyan: 36,
+//   gray: 90,
+//   lightRed: 91,
+//   lightGray: 37,
+//   lightGreen: 92,
+//   lightYellow: 93,
+//   lightBlue: 94,
+//   lightMagenta: 95,
+//   lightCyan: 96,
+// };
+// const LOG_FILE = (cname) => `./logs/${cname}.txt`;
+// const LOG_TARGETS = ['byme69', 'blackgeneralgenagenerator', 'latatatum'];
 
-const fileLog = (channel, tags, message) => {
-  if (LOG_TARGETS.includes(tags.username)) {
-    fs.appendFile(
-      LOG_FILE,
-      `<${currentTime()}>[channel:${channel}] ${tags.username} ${message}\n`,
-      (err) => {
-        if (err) {
-          console.log(err);
-          process.exit(0);
-        }
-      }
-    );
-  }
-};
+// function colorize(color, output) {
+//   return ['\033[', color, 'm', output, '\033[0m'].join('');
+// }
 
-const terminalLog = (channel, tags, message) => {
-  const formatted = `<${currentTime()}>[channel:${channel}] ${colorize(
-    TERM_COLORS.red,
-    tags.username
-  )} ${tags['message-type']}: ${message}\n`;
+// const currentTime = () =>
+//   new Date().toLocaleTimeString('en-US', { hour12: false });
 
-  process.stdout.write(formatted);
-};
+// const client = new tmi.Client({
+//   options: { debug: false },
+//   identity: {
+//     username: process.env.TWITCH_USERNAME,
+//     password: process.env.TWITCH_TOKEN,
+//   },
+//   channels: ['k0smos95'],
+// });
 
-client.connect().catch(console.error);
+// const fileLog = (channel, tags, message) => {
+//   if (LOG_TARGETS.includes(tags.username)) {
+//     fs.appendFile(
+//       LOG_FILE(channel),
+//       `<${currentTime()}>[channel:${channel}] ${tags.username} ${message}\n`,
+//       (err) => {
+//         if (err) {
+//           console.log(err);
+//           process.exit(0);
+//         }
+//       }
+//     );
+//   }
+// };
 
-client.on('message', (...args) => {
-  if (args.at(-1)) return;
-  terminalLog(...args);
-  fileLog(...args);
-});
-client.on('redeem', (channel, username, rewardType, tags, message) => {
-  console.log(
-    `<${currentTime()}>Triggered redeemed: ${username} -> ${rewardType}`
-  );
-});
-client.on('join', function (channel, username) {
-  console.log(`<${currentTime()}>Triggered join: <%s>`, username);
-});
+// const terminalLog = (channel, tags, message) => {
+//   const formatted = `<${currentTime()}>[channel:${channel}] ${colorize(
+//     TERM_COLORS.red,
+//     tags.username
+//   )} ${tags['message-type']}: ${message}\n`;
 
-client.on('notice', (channel, msgid, message) => {
-  console.log(`-------------------------`);
-  console.log(
-    `<${currentTime()}>Triggered notice: msgid -> %s, message -> %s `,
-    msgid,
-    message
-  );
-  console.log(`-------------------------`);
-});
+//   process.stdout.write(formatted);
+// };
+
+// client.connect().catch(console.error);
+
+// client.on('message', (...args) => {
+//   if (args.at(-1)) return;
+//   terminalLog(...args);
+//   fileLog(...args);
+// });
+
+// client.on('redeem', (channel, username, rewardType, tags, message) => {
+//   console.log(
+//     `<${currentTime()}>Triggered redeemed: ${username} -> ${rewardType}`
+//   );
+// });
+// client.on('join', function (channel, username) {
+//   console.log(`<${currentTime()}>Triggered join: <%s>`, username);
+// });
+
+// client.on('notice', (channel, msgid, message) => {
+//   console.log(`-------------------------`);
+//   console.log(
+//     `<${currentTime()}>Triggered notice: msgid -> %s, message -> %s `,
+//     msgid,
+//     message
+//   );
+//   console.log(`-------------------------`);
+// });
